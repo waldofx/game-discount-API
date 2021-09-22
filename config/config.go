1c5235f7f1fc5b1c087ecd26e7bc8f4787f64f65
Waldo <waldofelix1@gmail.com>
2021-09-22 21:49:02 +0700

    add folder structure and init config

---
+++ config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"discount-api/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	connectionString := "root:@tcp(0.0.0.0:3306)/project_temp?charset=utf8mb4&parseTime=True&loc=Local"
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func InitMigrate() {
+	DB.AutoMigrate(&models.User{})
+}
--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"discount-api/config"
+	ownMid "discount-api/middleware"
+	"discount-api/routes"
+)
+
+func main() {
+	config.InitDB()
+	config.InitMigrate()
+	e := routes.NewRoutes()
+	ownMid.LogMiddlewareInit(e)
+	e.Start(":8000")
+}
