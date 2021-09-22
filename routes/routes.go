fe35b48dcf7626e397b89fe4517240e45ea8657d
Waldo <waldofelix1@gmail.com>
2021-09-22 21:51:10 +0700

    add routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"discount-api/controllers"
+
+	"github.com/labstack/echo/v4"
+)
+
+func NewRoutes() *echo.Echo{
+	e := echo.New()
+	e.GET("/user/:id",controllers.GetUserController)
+	e.PUT("/user/:id",controllers.UpdateUserController)
+	e.DELETE("/user/:id",controllers.DeleteUserController)
+	e.GET("/user",controllers.GetAllUserController)
+	e.POST("/user",controllers.AddUserController)
+	return e
+}
